f08e4bbb16bb000b4f390f88eef5e02e707df6e5
AJ Olson <silentx.alex@gmail.com>
2020-10-29 15:47:44 -0400

    added light and dark world locations to map

--- src/map_overview/map.js
+++ src/map_overview/map.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-pascal-case */
 import React, {Component} from 'react';
 import LightWorld from './light';
 import DarkWorld from './dark';
+import LW_Locations from '../locations/lw_locations';
+import DW_Locations from '../locations/dw_locations';
 import LW_Map from '../assests/map/Zelda3LightOverworldBG.png';
 import DW_Map from '../assests/map/Zelda3DarkOverworldBG.png';
 
@@ -8,11 +11,13 @@ class Map extends Component {
     render() {
         return (
             <section className="map_overview">
-                <div>
+                <div className="lw">
                     <LightWorld LW_Map={LW_Map}/>
+                    <LW_Locations />
                 </div>
-                <div>
+                <div className="dw">
                     <DarkWorld DW_Map={DW_Map}/>
+                    <DW_Locations />
                 </div>
             </section>
         )
